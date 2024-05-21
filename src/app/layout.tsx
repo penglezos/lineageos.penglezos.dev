92d9a4663e55184253c473b490b5e0d0055a3f27
penglezos <panagiotisegl@gmail.com>
2024-05-21 20:24:48 +0300

    Import navbar component
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Navbar } from './components/nav'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto {inter.className}">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+          <Navbar />
           {children}
         </main>
       </body>
