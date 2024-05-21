2f226a402c212d56a1efb6837d15d2587d87f40f
penglezos <panagiotisegl@gmail.com>
2024-05-22 00:03:12 +0300

    Initial text changes
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,27 +4,15 @@ export default function Home() {
       <div className="flex justify-center items-center">
         <img src="lineage.png" width="300"></img>
       </div>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter text-center">
+      <h1 className="mb-12 text-4xl font-semibold tracking-tighter text-center">
         LineageOS builds by penglezos
       </h1>
+      <h2 className="mb-4 text-2xl ont-semibold tracking-tighter">What is LineageOS?</h2>
       <p className="mb-4">
-        {`Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-        when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-        It has survived not only five centuries, but also the leap into electronic typesetting, 
-        remaining essentially unchanged. It was popularised in the 1960s with the release of 
-        Letraset sheets containing Lorem Ipsum passages, 
-        and more recently with desktop publishing software like Aldus PageMaker 
-        including versions of Lorem Ipsum.
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-        when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-        It has survived not only five centuries, but also the leap into electronic typesetting, 
-        remaining essentially unchanged. It was popularised in the 1960s with the release of 
-        Letraset sheets containing Lorem Ipsum passages, 
-        and more recently with desktop publishing software like Aldus PageMaker 
-        including versions of Lorem Ipsum.`}
+        {`LineageOS Android Distribution is a free and open-source operating system for various devices, 
+        based on the Android mobile platform.`}
       </p>
+      <p className="mb-4">You can read more about LineageOS by visiting their <a href="https://lineageos.org" target="_blank" className="underline">official website.</a></p>
     </section>
   )
 }
