c26c7c0fd02017fc9b11c103a21860ea27fb59c5
penglezos <panagiotisegl@gmail.com>
2024-05-22 21:52:01 +0300

    Use Roboto font
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from './components/nav'
 import Footer from "./components/footer";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "LineageOS builds by penglezos",
@@ -18,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto {inter.className}">
+      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto font-Roboto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
           {children}
