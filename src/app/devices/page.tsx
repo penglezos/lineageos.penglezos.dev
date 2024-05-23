aa191c7145ced13fd247f38fe0b3c69560a75da1
penglezos <panagiotisegl@gmail.com>
2024-05-23 18:55:51 +0300

    Add devices page
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

---
+++ src/app/devices/page.tsx
@@ -0,0 +1,13 @@
+
+export const metadata = {
+  title: 'Devices',
+  description: 'Currently supported devices',
+}
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-12 text-4xl font-semibold tracking-tighter text-center">Supported Devices</h1>
+    </section>
+  )
+}
