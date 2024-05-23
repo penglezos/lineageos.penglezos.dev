5f82fe9b3efde07b2d0890af56a6e68aedefc039
penglezos <panagiotisegl@gmail.com>
2024-05-23 19:46:00 +0300

    Do not center text
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- src/app/devices/page.tsx
+++ src/app/devices/page.tsx
@@ -7,7 +7,7 @@ export const metadata = {
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-12 text-4xl font-semibold tracking-tighter text-center">Devices</h1>
+      <h1 className="mb-12 text-4xl font-semibold tracking-tighter">Devices</h1>
     </section>
   )
 }
