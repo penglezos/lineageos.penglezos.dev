dc2f8a2e7ef5ecdcb8a88b820224a9983f808db5
penglezos <panagiotisegl@gmail.com>
2024-05-23 19:45:33 +0300

    Supported Devices => Devices
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- src/app/devices/page.tsx
+++ src/app/devices/page.tsx
@@ -7,7 +7,7 @@ export const metadata = {
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-12 text-4xl font-semibold tracking-tighter text-center">Supported Devices</h1>
+      <h1 className="mb-12 text-4xl font-semibold tracking-tighter text-center">Devices</h1>
     </section>
   )
 }
